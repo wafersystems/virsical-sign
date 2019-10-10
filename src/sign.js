f9f24428b9224c8f78752f106538095187a3edbc
vkingw <alfa.king+git@gmail.com>
2019-10-10 19:36:47 +0800

    update version to 2.0.0

--- src/sign.js
+++ src/sign.js
@@ -1,34 +1,60 @@
-import { MD5 } from 'crypto-js';
+import md5 from 'crypto-js/md5';
 import parse from 'url-parse';
 
-export const getSign = (url, token) => {
+/**
+ *
+ * @param data
+ * @returns {*}
+ */
+const processJson = (data) => {
+  let signature = '';
+  data.forEach((item) => {
+    signature += item;
+  });
+  return md5(signature).toString();
+};
+
+/**
+ *
+ * @param timestamp
+ * @param url
+ * @param key
+ * @param body
+ * @returns {*}
+ */
+export const sign = (timestamp, url, key, body) => {
   const urlOjb = parse(url, true);
-  let paramList = [token || sessionStorage.getItem('token') || ''];
-  for (const p in urlOjb.query) {
-    paramList.push(`${p}${urlOjb.query[p]}`);
+  let paramList = [];
+  // 1 add key
+  paramList.push(key);
+  // 2 add timestamp
+  paramList.push(timestamp);
+  // 3 add path
+  paramList.push(urlOjb.pathname);
+  // 4 add query parameter
+  paramList.push('{');
+  const tmp = [];
+  Object.keys(urlOjb.query).forEach(key => {
+    tmp.push(`${key}=${urlOjb.query[key]}`);
+  });
+  paramList.push(tmp.toString());
+  paramList.push('}');
+  // 5 add body
+  if (body) {
+    paramList.push(body);
   }
   return processJson(paramList)
 };
 
-const parseStrPlus = (param) => {
-  let plus = 0;
-  for (let pos = 0; pos < param.length; pos++) {
-    plus += param.charCodeAt(pos);
-  }
-  return plus;
-};
 
-function processJson(data) {
-  let signature = 0;
-  data.forEach((item) => {
-    signature += parseStrPlus(getMd5(item));
-  });
-  return signature
-}
-
-const getMd5 = (str) => {
-  let md5Hex = MD5(str).toString();
-  return md5Hex.slice(8, 24)
+/**
+ *  get url sing with key.
+ * @param url
+ * @param key
+ * @param body
+ */
+export const getSign = (url, key, body) => {
+  return sign(new Date().getTime(), url, key, body);
 };
 
 let VirsicalSign = {};
--- test/sign.test.js
+++ test/sign.test.js
@@ -3,16 +3,28 @@
  * Date : 2019-01-25 11:22
  * Email : wangxiao@wafersystems.com
  */
-import VirsicalSign from '../src/sign';
-
+import VirsicalSign, { sign } from '../src/sign';
 
 const url = 'http://localhost:8080/api/user?';
 const parameterWithBlank = 'id=1&lang=zh_CN&tokenSn=1111&name= ABC';
 const parameter = 'id=1&lang=zh_CN&tokenSn=1111&name=ABC';
+const body = JSON.stringify({ a: 1, b: 2 });
 
-const token = 'ADFASA23427UIOUKIJO9W3YOHSLEFJS93U0RJIFSD';
+const key = '123';
 
 test('url中参数带空格和不带空格, 签名值应该不同.', () => {
-  expect(VirsicalSign.getSign(url + parameterWithBlank, token))
-    .not.toEqual(VirsicalSign.getSign(url + parameter, token));
+  expect(VirsicalSign.getSign(url + parameterWithBlank, key))
+    .not.toEqual(VirsicalSign.getSign(url + parameter, key));
+});
+
+test('携带body与不带body, 签名值应该不同.', () => {
+  expect(VirsicalSign.getSign(url + parameterWithBlank, key))
+    .not.toEqual(VirsicalSign.getSign(url + parameterWithBlank, key, body));
+});
+
+
+test('时间不同，参数相同，签名值不相同.', () => {
+  let time = new Date().getTime();
+  expect(sign(time, url + parameterWithBlank, key))
+    .not.toEqual(sign(time+1, url + parameterWithBlank, key));
 });
