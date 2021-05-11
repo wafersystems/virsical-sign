eae5a7486b811d74838b5dc829e03db51b6b3551
Wang Xiao <wangxiao@wafersystems.com>
2021-05-11 10:00:40 +0800

    modify update yarn.lock fix security vulnerability.

--- webpack.config.js
+++ webpack.config.js
@@ -8,19 +8,20 @@ module.exports = {
   entry: {
 	  sign: './src/sign.js'
   },
+  mode: "production",
   output: {
     path: path.join(__dirname, './dist'),
     filename: '[name].js',
     libraryTarget: "umd"
   },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.(js|jsx)$/,
         loader: 'babel-loader',
         include: [path.join(__dirname, './src')]
       }
-    ]
+    ],
   },
   externals: {
     "crypto-js": 'crypto-js'
